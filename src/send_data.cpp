88a6073ff468e5c5966d649cdb2f399bac3acd3e
Filip Jahn <xjahnf00@vutbr.cz>
2022-11-06 17:19:19 +0100

    Remove unnecesarry code

--- src/send_data.cpp
+++ src/send_data.cpp
@@ -24,15 +24,11 @@
 #include <iostream>
 
 
-
-#define BUFFER 1024 // buffer length
-
 int sock; // socket descriptor
 int msg_size, i;
 struct sockaddr_in server, from; // address structures of the server and the client
 struct hostent *servent;         // network host entry required by gethostbyname()
 socklen_t len, fromlen;
-char buffer[BUFFER];
 
 void create_connection(char *address, char *port)
 {
@@ -70,43 +66,19 @@ void close_connection()
 
 int send_data(struct flow flow)
 {
-  // memcpy(buffer, (const void*)&flow, sizeof(flow));
-  // send data to the server
-  //while ((msg_size = read(STDIN_FILENO, buffer, BUFFER)) > 0)
-  // read input data from STDIN (console) until end-of-line (Enter) is pressed
-  // when end-of-file (CTRL-D) is received, n == 0
-  //{
-    i = send(sock, (const void*)&flow, sizeof(flow), 0); // send data to the server
-    if (i == -1)                         // check if data was sent correctly
-      err(1, "send() failed");
-    else if (i != sizeof(flow))
-      err(1, "send(): buffer written partially");
-    else 
-      std::cout << "send() passed" << std::endl;
+  i = send(sock, (const void*)&flow, sizeof(flow), 0); // send data to the server
+  if (i == -1)                         // check if data was sent correctly
+    err(1, "send() failed");
+  else if (i != sizeof(flow))
+    err(1, "send(): flow written partially");
+  else 
+    std::cout << "send() passed" << std::endl;
 
-    // obtain the local IP address and port using getsockname()
-    if (getsockname(sock, (struct sockaddr *)&from, &len) == -1)
-      err(1, "getsockname() failed");
+  // obtain the local IP address and port using getsockname()
+  if (getsockname(sock, (struct sockaddr *)&from, &len) == -1)
+    err(1, "getsockname() failed");
 
-    printf("* Data sent from %s, port %d (%d) to %s, port %d (%d)\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port), from.sin_port, inet_ntoa(server.sin_addr), ntohs(server.sin_port), server.sin_port);
-
-    // read the answer from the server
-    // if ((i = recv(sock, buffer, BUFFER, 0)) == -1)
-    //   err(1, "recv() failed");
-    // else if (i > 0)
-    // {
-    //   // obtain the remote IP adddress and port from the server (cf. recfrom())
-    //   if (getpeername(sock, (struct sockaddr *)&from, &fromlen) != 0)
-    //     err(1, "getpeername() failed\n");
-
-    //   printf("* UDP packet received from %s, port %d\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port));
-    //   printf("%.*s", i, buffer); // print the answer
-    // }
-  // }
-  // reading data until end-of-file (CTRL-D)
-
-  // if (msg_size == -1)
-  //   err(1, "reading failed");
-
+  printf("* Data sent from %s, port %d (%d) to %s, port %d (%d)\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port), from.sin_port, inet_ntoa(server.sin_addr), ntohs(server.sin_port), server.sin_port);
+  
   return 0;
 }
